ff2a753fc8403f7f69308400298e841fd8a7f1c1
Ivan Eroshin <itseroshin@gmail.com>
2024-05-06 15:34:44 +0300

    LAB5 finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'work/index'
+  get 'work/choose_theme'
+  get 'work/display_theme'
   resources :images
   resources :themes
   resources :values
@@ -13,7 +16,14 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  # work
+  match 'work',                  to: 'work#index',                  via: 'get'
+  match 'choose_theme',     to: 'work#choose_theme',      via: :get
+  match 'display_theme',    to: 'work#display_theme',      via: :post
+
+
   # Defines the root path route ("/")
   # root "posts#index"
+  # root 'work#index'
   root 'main#index'
 end
